a318d5b5cc3989b43ce235cefe13610b0415e2ec
MaskerDog <37284851+MaskerDog@users.noreply.github>
2022-06-23 23:10:06 +0900

    Initial Commit

---
+++ CMVision/CMVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMVisionApp.swift
+//  CMVision
+//
+//  Created by npc on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CMVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
